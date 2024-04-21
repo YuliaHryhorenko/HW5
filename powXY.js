1deeeafe284808dd53214530fe6ca9a58090393b
Yuliia Hryhorenko <yuliiahryhorenko@gmail.com>
2024-04-21 14:37:34 +0300

    fix a mistake

--- powXY.js
+++ powXY.js
@@ -1,9 +1,18 @@
 function pow(x, y) {
   let result = 1;
-  for (let i = 0; i < y; i++) {
-    result = result * x;
+  if (y > 0) {
+    for (let i = 0; i < y; i++) { // підносимо до плюсового ступеня
+      result = result * x;
+    }
+    console.log(result);
+  } else if (y === 0) { // підносимо до 0
+    console.log(result);
+  } else {
+    for (let i = 0; i < -y; i++) { // підносимо до мінусового ступеня
+      result = result / x;
+    }
+    console.log(result);
   }
-  console.log(result);
 }
 
 pow(2, 3);
