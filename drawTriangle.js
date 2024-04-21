a207c657f926d9f6522f5208eb939bce3b934d12
Yuliia Hryhorenko <yuliiahryhorenko@gmail.com>
2024-04-21 14:56:11 +0300

    fix a mistake

--- drawTriangle.js
+++ drawTriangle.js
@@ -1,10 +1,29 @@
+// 1 way
+
 function drawTriangle(height, symbol) {
-    for (let rowNumber = 1; rowNumber <= height; rowNumber++) {
-        let line = '';
-        for (let symbolNumber = 0; symbolNumber < rowNumber; symbolNumber++) {
-            line = line + symbol;
-        }
-        console.log(line);
+  for (let rowNumber = 1; rowNumber <= height; rowNumber++) {
+    let line = "";
+    for (let symbolNumber = 0; symbolNumber < rowNumber; symbolNumber++) {
+      line = line + symbol;
     }
+    console.log(line);
+  }
 }
+drawTriangle(5, "*");
+
+// 2 way
+
+function drawTriangle(height, symbol, rowNumber = 1) {
+  if (rowNumber > height) {
+    return;
+  }
+
+  let line = "";
+  for (let symbolNumber = 0; symbolNumber < rowNumber; symbolNumber++) {
+    line = line + symbol;
+  }
+  console.log(line);
+  drawTriangle(height, symbol, rowNumber + 1);
+}
+
 drawTriangle(5, "*");
